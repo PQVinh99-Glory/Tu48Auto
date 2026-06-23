9c2120556af1e6c7c6af6acf6de0470de697c6a2
0334896628 <0334896628@github.com>
2026-06-23 17:27:19 +0700

    Enlarge social icons in HeroIntro

--- src/components/HeroIntro.tsx
+++ src/components/HeroIntro.tsx
@@ -70,7 +70,7 @@ export function HeroIntro() {
 
       {/* Nội dung Text */}
       <div className="hero-copy absolute right-[7vw] top-[22vh] z-20 max-w-[800px] text-right max-lg:bottom-28 max-lg:left-5 max-lg:right-5 max-lg:top-auto max-lg:text-left">
-        <div className="mb-4 flex items-center justify-end gap-4 max-lg:justify-start">
+        <div className="mb-6 flex items-center justify-end gap-6 max-lg:justify-start">
           {/* Facebook */}
           <a
             href="https://www.facebook.com/people/T%C3%BA-Suzuki-Mi%E1%BB%81n-Nam/100090847749271/"
@@ -78,7 +78,7 @@ export function HeroIntro() {
             rel="noreferrer"
             className="text-white/70 transition hover:text-blue-500 hover:scale-110"
           >
-            <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+            <svg className="w-8 h-8" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
               <path fillRule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clipRule="evenodd" />
             </svg>
           </a>
@@ -90,7 +90,7 @@ export function HeroIntro() {
             rel="noreferrer"
             className="text-white/70 transition hover:text-white hover:scale-110"
           >
-            <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+            <svg className="w-8 h-8" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
               <path d="M12.525.02c1.31-.02 2.61-.01 3.91-.02.08 1.53.63 3.09 1.75 4.17 1.12 1.11 2.7 1.62 4.24 1.79v4.03c-1.44-.05-2.89-.35-4.2-.97-.57-.26-1.1-.59-1.62-.93-.01 2.92.01 5.84-.02 8.75-.08 2.78-1.15 5.54-3.33 7.35-2.22 1.83-5.27 2.26-8.05 1.5-2.68-.74-4.96-2.5-6.05-5.1-.96-2.28-1-4.95-.08-7.28 1.11-2.8 3.52-4.95 6.43-5.6 2.3-.51 4.79-.31 6.95.66v4.07c-1.35-.91-3.07-1.18-4.63-.8-1.54.38-2.85 1.39-3.56 2.81-.66 1.34-.69 2.95-.14 4.31.57 1.41 1.75 2.5 3.19 2.96 1.46.47 3.15.34 4.51-.43 1.34-.76 2.27-2.06 2.53-3.6.09-.54.12-1.09.11-1.63-.03-3.52-.02-7.05-.03-10.57z" />
             </svg>
           </a>
@@ -102,7 +102,7 @@ export function HeroIntro() {
             rel="noreferrer"
             className="text-white/70 transition hover:text-red-500 hover:scale-110"
           >
-            <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+            <svg className="w-8 h-8" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
               <path fillRule="evenodd" d="M19.812 5.418c.861.23 1.538.907 1.768 1.768C21.998 8.746 22 12 22 12s0 3.255-.418 4.814a2.504 2.504 0 0 1-1.768 1.768c-1.56.419-7.814.419-7.814.419s-6.255 0-7.814-.419a2.505 2.505 0 0 1-1.768-1.768C2 15.255 2 12 2 12s0-3.255.417-4.814a2.507 2.507 0 0 1 1.768-1.768C5.744 5 11.998 5 11.998 5s6.255 0 7.814.418ZM15.194 12 10 15V9l5.194 3Z" clipRule="evenodd" />
             </svg>
           </a>
@@ -115,7 +115,7 @@ export function HeroIntro() {
             className="text-white/70 transition hover:text-blue-400 hover:scale-110 ml-1"
             title="Zalo: 033.489.6628"
           >
-            <svg className="w-6 h-6" viewBox="0 0 24 24" fill="currentColor">
+            <svg className="w-9 h-9" viewBox="0 0 24 24" fill="currentColor">
               <path d="M21.547 7.031c-1.464-3.616-5.875-5.513-9.58-4.482-3.801 1.056-6.49 4.382-6.49 8.28 0 1.942.664 3.75 1.83 5.176.104.127.172.284.195.448l.385 2.766c.074.526.685.79 1.109.48l2.645-1.932c.16-.118.358-.172.556-.153 1.054.103 2.133.029 3.19-.228 3.73-.912 6.516-3.929 6.848-7.747.165-1.921-.247-3.83-1.688-5.352zm-12.753 6.945c-.477 0-.863-.386-.863-.863s.386-.863.863-.863.863.386.863.863-.386.863-.863.863zm2.59-1.938c-.476 0-.862-.386-.862-.863s.386-.863.863-.863.863.386.863.863-.387.863-.863.863zm2.589 0c-.476 0-.862-.386-.862-.863s.386-.863.863-.863.863.386.863.863-.387.863-.863.863zm2.589 0c-.476 0-.862-.386-.862-.863s.386-.863.863-.863.863.386.863.863-.387.863-.863.863z"/>
             </svg>
           </a>
